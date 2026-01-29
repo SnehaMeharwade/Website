3758663a12f3ad16487dc099d081edd0e87dd128
SnehaMeharwade <85690578+SnehaMeharwade@users.noreply.github.com>
2026-01-30 01:41:00 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const app = express();
-
-app.use(cors({
-  origin: [
-    "https://697bb70d4e69b88edd8ca36c--hrmslite4.netlify.app","https://hrmslite6.netlify.app/","https://hrmslite4.netlify.app/"
-  ]
-}));
-
-app.use(express.json());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
